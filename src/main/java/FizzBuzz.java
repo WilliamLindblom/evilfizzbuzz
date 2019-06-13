3e54a3e1d1a1b0f2d961e2e325dc7b2240173be8
“William <wwwilliam.lindblom@gmail.com>
2019-06-13 11:56:50 +0200

    Separate with commas

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -26,4 +26,13 @@ public class FizzBuzz {
 		int parsedNumber = Integer.parseInt(numberString);
 		return (parsedNumber % 3 == 0) && (parsedNumber % 5 == 0) ? "FizzBuzz" : numberString;
 	}
+
+	public static String separateWithCommas(int max) {
+    	StringBuilder result = new StringBuilder();
+    	result.append(1);
+    	for (int i = 2; i <= max; i++) {
+    		result.append(",").append(i);
+	    }
+    	return result.toString();
+	}
 }
--- src/test/java/NumbersTest.java
+++ src/test/java/NumbersTest.java
@@ -1,10 +1,21 @@
-import static junit.framework.TestCase.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.parameterized.BlockJUnit4ClassRunnerWithParameters;
 
+@RunWith(JUnitParamsRunner.class)
 public class NumbersTest {
 	@Test
-	public void dummyTest() {
-		assertTrue(true);
+	@Parameters({
+			"3,1\\,2\\,3",
+			"4,1\\,2\\,3\\,4",
+	})
+	public void createCommaSeparatedString(int max, String expected) {
+		String commaSeparatedNumbers = FizzBuzz.separateWithCommas(max);
+		assertThat(commaSeparatedNumbers).isEqualTo(expected);
 	}
 }
